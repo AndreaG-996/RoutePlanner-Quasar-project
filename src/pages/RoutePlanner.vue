22ba7c561deb7774dee32a04caa3d435a694bfbc
Andrea GUO <andrea.guxying@gmail.com>
2024-12-16 13:54:23 +0000

    Fix Leaflet marker path for deployment

--- src/pages/RoutePlanner.vue
+++ src/pages/RoutePlanner.vue
@@ -50,6 +50,13 @@ export default {
   },
   mounted() {
     this.initMap()
+    // 修复 Leaflet 图标路径
+    delete L.Icon.Default.prototype._getIconUrl
+    L.Icon.Default.mergeOptions({
+      iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+      iconUrl: require('leaflet/dist/images/marker-icon.png'),
+      shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+    })
   },
   methods: {
     initMap() {
